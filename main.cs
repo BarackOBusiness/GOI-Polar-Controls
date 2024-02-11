150c380fee9937ff999ae9bc208992ce860750a3
BarackOBusiness <nichjoh21@gmail.com>
2024-02-10 19:28:50 -0500

    Reworked from replacing player controller to patching original controller
    Mod structure vastly simplified as I figured out how to patch methods without running the original code.
    This update also fixes the bug where the player controller is lost when jumping into the lake,
    and also possibly others I experienced while playing.

--- main.cs
+++ main.cs
@@ -1,239 +1,57 @@
-﻿// I don't want the mod to display "Getting Over It with Barack OBusiness" on the title screen for player builds
-#define RELEASE
-
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Configuration;
-using System.Collections;
+using HarmonyLib;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-#if !RELEASE
-	using TMPro;
-#endif
 
 namespace polar;
 
-[BepInPlugin("goiplugins.ext.polarcoordinates", "Polar Coordinate Inputs", "1.0.0")]
+[BepInPlugin("goiplugins.ext.polarcoordinates", "Polar Coordinate Input Scheme", "2.0.0")]
 public class PolarCoordinates : BaseUnityPlugin
 {
-	ConfigEntry<float> RotationSens;
-	ConfigEntry<float> SliderSens;
+	static ConfigEntry<float> rotationSens;
+	static ConfigEntry<float> sliderSens;
 
-    private void Awake()
-    {
-		RotationSens = Config.Bind("", "Rotation Sensitivity", 1f, "How sensitive the hinge joint is to your mouse input.");
-		SliderSens = Config.Bind("", "Slider Sensitivity", 1f, "How sensitive the slider is to your mouse inputs.");
-	
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    
-        Debug.Log("Welcome to a world of suffering :)");
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-		#if !RELEASE
-        	if (scene.name == "Loader") {
-				GameObject Author = GameObject.Find("Canvas/Mask/Title/Author");
-				Author.GetComponent<TextMeshProUGUI>().text = "with Barack OBusiness";
-			}
-		#endif
-		if (scene.name == "Mian") {
-			PlayerControl OrigController = GameObject.Find("Player").GetComponent<PlayerControl>();
-			OrigController.enabled = false;
-			Component.Destroy(OrigController);
-			PlayerControl2 Controls = GameObject.Find("Player").AddComponent<PlayerControl2>();
-			Controls.Init(RotationSens.Value, SliderSens.Value);
-		}
-    }
-}
-
-class PlayerControl2 : MonoBehaviour {
-	public HingeJoint2D hj;
-	public SliderJoint2D sj;
-
-	private JointMotor2D motor;
-	private JointMotor2D slider;
-
-	private float HingeSensitivity = 3200f;
-	private float SliderSensitivity = 25f;
-
-	private float pauseInputTimer;
-
-	public bool loadedFromSave;
-
-	public bool loadFinished;
-
-	// Necessary for ghost hammer... whatever that is
-	//private PolygonCollider2D hammerCollider;
-	//private List<Collider2D> ghostedCols;
-
-	private PoseControl pose;
-
-	private int numWins;
-
-	public void Init(float hingeSens, float sliderSens) {
-		HingeSensitivity = hingeSens * 3200f; // A reasonable default sensitivity 
-		SliderSensitivity = sliderSens * 25f; // A reasonable default sensitivity here too
-	}
+	static bool destroyedCursor = false;
 
 	private void Awake() {
-		loadedFromSave = false;
-		loadFinished = false;
+		rotationSens = Config.Bind("", "Hinge Sensitivity", 1.0f, "How sensitive the hammer's rotation is to your mouse input");
+		sliderSens = Config.Bind("", "Slider Sensitivity", 1.0f, "How sensitive the hammer's extension is to your mouse input");
+
+		Logger.LogInfo("Polar Input Scheme has loaded, welcome to pain :)");
+
+		Harmony.CreateAndPatchAll(typeof(PolarCoordinates));
 	}
 
-	private void Start() {
-		hj = GameObject.Find("Player/Hub").GetComponent<HingeJoint2D>();
-		motor = hj.motor;
-		motor.motorSpeed = 0f;
+	[HarmonyPatch(typeof(PlayerControl), "FixedUpdate")]
+	[HarmonyPrefix]
+	static bool RealFixedUpdate(PlayerControl __instance, ref JointMotor2D ___motor, ref JointMotor2D ___slider) {
+		if (!destroyedCursor && __instance.fakeCursor.gameObject != null) {
+			Component.Destroy(__instance.fakeCursor.GetComponent<SpriteRenderer>());
+			destroyedCursor = true;
+		}
 		
-		sj = GameObject.Find("Player/Hub/Slider").GetComponent<SliderJoint2D>();
-		slider = sj.motor;
-		slider.motorSpeed = 0f;
-		// For some reason max torque gets set to 4300 without this line, this restores it to default.
-		slider.maxMotorTorque = 10000f;
-
-		GameObject.Destroy(GameObject.Find("Cursor"));
-		
-		numWins = PlayerPrefs.GetInt("NumWins", 0);
-		//ghostedCols = new List<Collider2D>();
-		//hammerCollider = tip.GetComponent<PolygonCollider2D>();
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-		pauseInputTimer = 2f;
-		Debug.Log("enabling input");
-	}
-
-	private void OnApplicationFocus(bool focus) {
-		if (focus)
-		{
-			StartCoroutine(LockCursor());
+		if (!__instance.loadFinished) {
+			return false;
 		}
+	
+		// The 3200 and 25 here are good default sensitivity values for a ~1500 dpi mouse
+		// so sensitivity config values are just a multiplier of that
+		float mouseX = Input.GetAxis("Mouse X") * 3200f * rotationSens.Value;
+		float mouseY = Input.GetAxis("Mouse Y") * 25f * sliderSens.Value;
+
+		float motorSpeed = CircularLerp(___motor.motorSpeed, mouseX, 1.0f / 3.0f);
+		___motor.motorSpeed = -Mathf.Clamp(motorSpeed, -800f, 800f);
+		__instance.hj.motor = ___motor;
+
+		motorSpeed = CircularLerp(___slider.motorSpeed, mouseY, 0.125f);
+		___slider.motorSpeed = Mathf.Clamp(motorSpeed, -50f, 50f);
+		__instance.sj.motor = ___slider;
+
+		return false;
 	}
 
-	private IEnumerator LockCursor() {
-		yield return new WaitForSeconds(0.1f);
-		Cursor.lockState = CursorLockMode.Locked;
-	}
-
-	//private void GhostHammer(PolygonCollider2D otherCol) {
-	//	ghostedCols.Add(otherCol);
-	//	Physics2D.IgnoreCollision(hammerCollider, otherCol);
-	//}
-
-	public void SetSensitivity(float newSensitivity) {
-		// Do nothing, since sensitivity isn't valid here
-	}
-
-	public void PauseInput(float timeToPause) {
-		pauseInputTimer = timeToPause * 2f;
-		if (timeToPause < 0f)
-		{
-			Debug.Log("disabling input");
-		}
-		else if (timeToPause == 0f)
-		{
-			Debug.Log("enabling input");
-		}
-	}
-
-	public void StopAnimator() {
-		if (pose == null)
-		{
-			pose = GetComponentInChildren<PoseControl>();
-		}
-		pose.StopAnimator();
-	}
-
-	public void StartAnimator() {
-		if (pose == null)
-		{
-			pose = GetComponentInChildren<PoseControl>();
-		}
-		pose.StartAnimator();
-	}
-
-	public void PlayOpeningAnimation() {
-		GetComponentInChildren<PoseControl>().PlayOpeningAnimation();
-	}
-
-	private void Update() {
-		if (numWins > 0 && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKey(KeyCode.R))
-		{
-			PlayerPrefs.DeleteKey("NumSaves");
-			PlayerPrefs.DeleteKey("SaveGame0");
-			PlayerPrefs.DeleteKey("SaveGame1");
-			PlayerPrefs.Save();
-			SceneManager.LoadScene("Mian");
-		}
-	}
-
-	private void FixedUpdate() {
-		float mouseX = Input.GetAxis("Mouse X") * HingeSensitivity;
-		float mouseY = Input.GetAxis("Mouse Y") * SliderSensitivity;
-
-		float motorSpeed = CircularLerp(motor.motorSpeed, mouseX, 1.0f / 3.0f);
-		float sliderSpeed = CircularLerp(slider.motorSpeed, mouseY, 0.125f);
-		
-		motor.motorSpeed = -Mathf.Clamp(motorSpeed, -800f, 800f);
-		hj.motor = motor;
-
-		slider.motorSpeed = Mathf.Clamp(sliderSpeed, -50f, 50f);
-		sj.motor = slider;
-	}
-
-	private float CircularLerp(float start, float end, float t) {
+	static float CircularLerp(float start, float end, float t) {
 		float factor = Mathf.Sin(0.5f * Mathf.PI * t);
 		return start + ((end - start) * factor);
 	}
 }
-
-// The reference implementation of the polar coordinates control scheme
-//class ReferenceImpl : MonoBehaviour {
-//	public HingeJoint2D hj;
-//	public SliderJoint2D sj;
-//
-//	private JointMotor2D motor;
-//	private JointMotor2D slider;
-//
-//	private float HingeSensitivity = 1000f;
-//	private float SliderSensitivity = 250f;
-//
-//	private float lastX;
-//	private float currentX;
-//	private float lastY;
-//	private float currentY;
-//
-//	public void Init(float hingeSens, float sliderSens) {
-//		HingeSensitivity = hingeSens * 2000f;
-//		SliderSensitivity = sliderSens * 50f;
-//	}
-//
-//	private void Awake() {
-//		hj = GameObject.Find("Player/Hub").GetComponent<HingeJoint2D>();
-//		sj = GameObject.Find("Player/Hub/Slider").GetComponent<SliderJoint2D>();
-//
-//		motor = hj.motor;
-//		
-//		slider = sj.motor;
-//		slider.maxMotorTorque = 10000f;
-//	}
-//
-//	private void FixedUpdate() {
-//		lastX = currentX;
-//		lastY = currentY;
-//		currentX = Input.GetAxis("Mouse X") * HingeSensitivity;
-//		currentY = Input.GetAxis("Mouse Y") * SliderSensitivity;
-//
-//		float interX = Mathf.Lerp(lastX, currentX, 0.5f);
-//		float interY = Mathf.Lerp(lastY, currentY, 0.5f);
-//		
-//		motor.motorSpeed = -Mathf.Clamp(interX, -800f, 800f);
-//		hj.motor = motor;
-//
-//		slider.motorSpeed = Mathf.Clamp(interY, -50f, 50f);
-//		sj.motor = slider;
-//
-//		// Distance:
-//		//	[-1.1,0.7]
-//		// Don't think I need this anymore
-//	}
-//}
-

76047b31f2e56e62271bcbeacc202c271c9f0255
BarackOBusiness <nichjoh21@gmail.com>
2024-02-20 16:39:53 -0500

    Updated to new mod specification
    Current spec on a compliant mod implementation is unwritten, however as of right now what has been introduced is the description field for the mod, and the ConfigurationItem struct has gone away in favor of an attribute.
    Additionally this update fixes the allowed motion during the player's spawn animation.

--- PolarCoordinates.cs
+++ PolarCoordinates.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using GOIModManager.Core;
+﻿using GOIModManager.Core;
 using Newtonsoft.Json;
 using HarmonyLib;
 using UnityEngine;
@@ -9,6 +8,7 @@ namespace PolarCoordinates;
 
 public class PolarCoordinates : IMod {
 	public string Name { get; } = "Polar Coordinates"; // A user friendly name for the mod
+	public string Description { get; } = "A challenge mod that replaces the input scheme of the game.\nYour mouse's x axis will control the rotation of the hammer, and the y axis the extension of the hammer.\nThis is as opposed to controlling a cursor that the hammer simply follows.";
 	public ModConfiguration Configuration { get; private set; } = new PolarCoordinatesConfig();
 
 	private Harmony patcher;
@@ -45,9 +45,23 @@ public class PolarCoordinates : IMod {
 public class MyPatches {
 	[HarmonyPatch(typeof(PlayerControl), "FixedUpdate")]
 	[HarmonyPrefix]
-	static bool FixedUpdate(PlayerControl __instance, ref Player ___player, ref JointMotor2D ___motor, ref JointMotor2D ___slider) {
-		float mouseX = ___player.GetAxis("mouseX") * 400f * PolarCoordinatesConfig.RotationSens.Value;
-		float mouseY = ___player.GetAxis("mouseY") * 1.5625f * PolarCoordinatesConfig.SliderSens.Value;
+	static bool FixedUpdate(
+		PlayerControl __instance, 
+		ref Player ___player, 
+		ref JointMotor2D ___motor, 
+		ref JointMotor2D ___slider,
+		ref float ___pauseInputTimer
+	) {
+		if (___pauseInputTimer > 0f) {
+			___pauseInputTimer -= Time.fixedDeltaTime * 2f;
+			if (___pauseInputTimer < 0f) {
+				___pauseInputTimer = 0f;
+			}
+			return false;
+		}
+	
+		float mouseX = ___player.GetAxis("mouseX") * 400f * PolarCoordinatesConfig.RotationSens;
+		float mouseY = ___player.GetAxis("mouseY") * 1.5625f * PolarCoordinatesConfig.SliderSens;
 
 		float motorSpeed = Utils.CircularLerp(___motor.motorSpeed, mouseX, 1.0f / 3.0f);
 		___motor.motorSpeed = -Mathf.Clamp(motorSpeed, -800f, 800f);
@@ -62,24 +76,15 @@ public class MyPatches {
 }
 
 public class PolarCoordinatesConfig : ModConfiguration {
-	public override bool IsEnabled { get; set; }
+	public override bool IsEnabled { get; set; } = true;
 
+	[ConfigurationItem("Rotation Sensitivity", "How sensitive the hammer's rotation is to your mouse input")]
 	[JsonProperty("Rotation Sensitivity")]
-	public static ConfigurationItem<float> RotationSens;
+	public static float RotationSens = 1.0f;
+	
+	[ConfigurationItem("Slider Sensitivity", "How sensitive the hammer's extension is to your mouse input")]
 	[JsonProperty("Slider Sensitivity")]
-	public static ConfigurationItem<float> SliderSens;
-
-	// [JsonProperty]
-	// public static float RotationSens;
-	// [JsonProperty]
-	// public static float SliderSens;
-
-	public PolarCoordinatesConfig() {
-		IsEnabled = true;
-		RotationSens = new ConfigurationItem<float>("How sensitive the hammer's rotation is to your mouse input", 1.0f);
-		SliderSens = new ConfigurationItem<float>("How sensitive the hammer's extension is to your mouse input", 1.0f);
-		Debug.Log("Constructed polar coordinates mod config");
-	}
+	public static float SliderSens = 1.0f;
 }
 
 public static class Utils {
